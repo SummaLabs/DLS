f9436f0a0339ade9864d8f2cca6998feeb6fac35
sergo <sergokov@gmail.com>
2016-07-28 21:00:50 +0300

    Share nodes data between controllers temp

--- app/frontend/components/builder/constructor/core.js
+++ app/frontend/components/builder/constructor/core.js
@@ -2,8 +2,8 @@
 
 
 angular.module('constructorCore', [
-  'palette',
-  'graph'
+	'palette',
+	'graph'
 ]);
 
 var editorDefinition = {
@@ -13,94 +13,122 @@ var editorDefinition = {
 	bindings: {
 
 	}
-}
+};
 
 angular.module('constructorCore')
-	.service('constructorService', ConstructorService)
+	.service('constructorLoaderService', ConstructorLoaderService)
+	.service('constructorService', ['constructorLoaderService', ConstructorDataService])
 	.component('constructor', editorDefinition);
 
-function ConstructorService() {
-	var categories = [
-		{
-        	name : 'input',
-    	},
-    	{
-        	name : 'output',
-    	},
-    ];
+function ConstructorDataService(constructorLoaderService) {
+	var categories = constructorLoaderService.loadLayerCategories();
 
-    var paletteElements = [
-		{
-		    id: 1,
-			name : 'websocket',
-			content : 'web',
-			category : 'input',
-			pos: {x: 100, y: 200},
-			selected: false
-		}, {
-		    id: 2,
-			name : 'socket',
-			content : 'socket',
-			category : 'input',
-			pos: {x: 300, y: 300},
-			selected: false
-		}, {
-		    id: 3,
-			name : 'db',
-			content : 'db',
-			category : 'output',
-			pos: {x: 300, y: 500},
-			selected: false
-		},
-	];
+	var paletteElements = constructorLoaderService.loadNetworksLayers();
 
-  	var nodes = [
-		{
-		    id: 0,
-			name : 'websocket',
-			content : 'web',
-			category : 'input',
-			pos: {x: 100, y: 200},
-			wires: [
-			    1
-			]
-		}, {
-		    id: 1,
-			name : 'socket',
-			content : 'socket',
-			category : 'input',
-			pos: {x: 300, y: 300},
-			wires: [
-			    2, 0
-			]
-		}, {
-		    id: 2,
-			name : 'db',
-			content : 'db',
-			category : 'output',
-			pos: {x: 300, y: 100},
-		},
-	];
+	var nodes = constructorLoaderService.loadSavedNetwork();
 
-    this.getCategories= function() {
-    	return categories;
-    };
+	this.getCategories= function() {
+		return categories;
+	};
 
-    this.getPaletteElements= function() {
-    	return paletteElements;
-    };
+	this.getPaletteElements= function() {
+		return paletteElements;
+	};
 
 	this.getNodes = function() {
-    	return nodes;
-    };
+		return nodes;
+	};
+
+	this.addNode = function(node) {
+		nodes.push(node);
+	};
+
+	this.updateNode = function(node) {
+		nodes.push(node);
+	};
 }
 
-function ConstructorController($scope, $rootScope, $element) {
+function ConstructorLoaderService() {
+
+	this.loadSavedNetwork = function () {
+		var network = [
+			{
+				id: 0,
+				name : 'websocket',
+				content : 'web',
+				category : 'input',
+				pos: {x: 100, y: 200},
+				wires: [
+					1
+				]
+			}, {
+				id: 1,
+				name : 'socket',
+				content : 'socket',
+				category : 'input',
+				pos: {x: 300, y: 300},
+				wires: [
+					2, 0
+				]
+			}, {
+				id: 2,
+				name : 'db',
+				content : 'db',
+				category : 'output',
+				pos: {x: 300, y: 100},
+			}
+		];
+
+		return network
+	};
+
+	this.loadNetworksLayers = function () {
+		var networksLayers = [
+			{
+				id: 1,
+				name : 'websocket',
+				content : 'web',
+				category : 'input',
+				pos: {x: 100, y: 200},
+				selected: false
+			}, {
+				id: 2,
+				name : 'socket',
+				content : 'socket',
+				category : 'input',
+				pos: {x: 300, y: 300},
+				selected: false
+			}, {
+				id: 3,
+				name : 'db',
+				content : 'db',
+				category : 'output',
+				pos: {x: 300, y: 500},
+				selected: false
+			}
+		];
+
+		return networksLayers;
+	};
+
+	this.loadLayerCategories = function () {
+
+		var categories = [
+			{
+				name : 'input',
+			},
+			{
+				name : 'output',
+			}
+		];
+
+		return categories;
+	}
+}
+
+function ConstructorController() {
 
 	this.$onInit = function() {
 
-    };
+	};
 }
-
-
-
