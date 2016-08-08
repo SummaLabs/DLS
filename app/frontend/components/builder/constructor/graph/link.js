b09522c96afd25f2d8fd0898c406b5120eee8451
Dzianis Sushchenia <suschenya@gmail.com>
2016-08-08 18:17:37 +0300

    fix

---
+++ app/frontend/components/builder/constructor/graph/link.js
@@ -0,0 +1,128 @@
+'use strict';
+
+angular.module('graph')
+	.directive('nodelink', link);
+
+function link() {
+	var from, to;
+
+	return {
+		restrict: 'E',
+		controller: LinkCtrl,
+		controllerAs: 'ln',
+		replace: true,
+		scope: {
+		   linkData: '=',
+		},
+		template: '<path  class="link_line" d=""/>',
+		templateNamespace: 'svg',
+
+		link: function($scope, element, attrs) {
+
+			from = {
+				x: $scope.linkData.nodes[0].pos.x + $scope.linkData.nodes[0].portOut.offset.x,
+				y: $scope.linkData.nodes[0].pos.y + $scope.linkData.nodes[0].portOut.offset.y,
+			}
+
+			if($scope.linkData.nodes[1].id === 'activePoint') {
+				to = $scope.linkData.nodes[1].pos;
+			} else {
+				to = {
+					x: $scope.linkData.nodes[1].pos.x + $scope.linkData.nodes[1].portIn.offset.x,
+					y: $scope.linkData.nodes[1].pos.y + $scope.linkData.nodes[1].portIn.offset.y,
+				}
+			}
+			linkWatcher($scope, element);
+			eventsHandler($scope, element);
+
+			console.log($scope.linkData.nodes[0], $scope.linkData.nodes[1]);
+		}
+	}
+
+	function linkWatcher(scope, element) {
+		scope.$watch('linkData.selected', function(newValue, oldValue) {
+			if (newValue) {
+				element.addClass("link_line_active");
+				element.attr('stroke-dasharray', '5,5');
+			} else {
+				element.removeClass("link_line_active");
+				element.attr('stroke-dasharray', '');
+			}
+		});
+
+		scope.$watch('linkData.nodes[0].pos.x', function(newValue, oldValue) {
+			from.x += newValue - oldValue;
+			updatePos(element, from, to);
+		});
+
+		scope.$watch('linkData.nodes[0].pos.y', function(newValue, oldValue) {
+			from.y += newValue - oldValue;
+			updatePos(element, from, to);
+		});
+
+		scope.$watch('linkData.nodes[1].pos.x', function(newValue, oldValue) {
+			to.x += newValue - oldValue;
+			updatePos(element, from, to);
+		});
+
+		scope.$watch('linkData.nodes[1].pos.y', function(newValue, oldValue) {
+			to.y += newValue - oldValue;
+			updatePos(element, from, to);
+		});
+	}
+
+	function eventsHandler(scope, element) {
+		element.on('mouseenter', function (event) {
+			element.addClass("link_line_active");
+		});
+
+		element.on('mouseleave', function (event) {
+			if (!scope.linkData.selected) {
+				element.removeClass("link_line_active");
+			}
+		});
+
+		element.on('click', function (event) {
+			if (!event.ctrlKey)
+				return;
+			scope.$apply( function() {
+				scope.linkData.selected = !scope.linkData.selected;
+			});
+			scope.$emit('selectedItem', {
+				id: scope.linkData.id,
+				type: 'link',
+				selected: scope.linkData.selected
+			});
+		});
+	}
+
+	function updatePos(element, from, to){
+		var path = calculatePath(from, to);
+		element.attr('d', path);
+	}
+
+	function calculatePath(from, to) {
+		var ARC_OFFSET = 0.3;
+		var ARC_MIN = 30;
+		var points = [];
+		var dx = to.x - from.x;
+		var dy = to.y - from.y;
+
+		var distance = Math.max(Math.sqrt(dx * dx + dy * dy) * ARC_OFFSET, ARC_MIN);
+		points.push(from);
+		points.push({x: from.x + distance, y: from.y});
+		points.push({x: to.x - distance, y: to.y});
+		points.push(to);
+
+		var linkFunction = d3.line()
+			.x(function(d) { return d.x; })
+			.y(function(d) { return d.y; })
+			.curve(d3.curveBasis);
+
+		return linkFunction(points);
+	}
+}
+
+function LinkCtrl($scope, $element, $document) {
+
+}
---
+++ app/frontend/components/builder/constructor/graph/node.js
@@ -0,0 +1,225 @@
+'use strict';
+
+angular.module('graph')
+	.directive('node', node);
+
+function node($compile, $templateCache, $http) {
+
+	var patternDefinitions;
+	var isPort = false;
+
+	function NodeCtrl($scope, $element, $document, graphService) {
+
+		this.$onInit = function() {
+			patternDefinitions = graphService.patternDefinitions;
+		};
+	}
+
+	return {
+		restrict: 'E',
+		controller: NodeCtrl,
+		controllerAs: 'nd',
+		replace: true,
+		scope: {
+		   nodeData: '=',
+		},
+		template: '<g ng-attr-transform="translate({{nodeData.pos.x}},{{nodeData.pos.y}})" width="100%" height="100%"></g>',
+		templateNamespace: 'svg',
+
+
+		link: function($scope, element, attrs) {
+            $scope.$watch('nodeData.template', function(newType) {
+                if (newType != "" && newType != undefined) {
+                    $http.get(newType, {cache: $templateCache}).success(function(html) {
+                        element.html(html);
+                        $compile(element.contents())($scope);
+
+                        var idNode = $scope.nodeData.id;
+
+                        var rectNode = angular.element(element[0].querySelector('#' + patternDefinitions.markerRect));
+                        var textNode = angular.element(element[0].querySelector('#' + patternDefinitions.markerText));
+                        var portIn = portInit(
+							element,
+							angular.element(element[0].querySelector('#' + patternDefinitions.markerPortIn)),
+							$scope.nodeData,
+							patternDefinitions.markerPortIn
+						);
+                        var portOut = portInit(
+							element,
+							angular.element(element[0].querySelector('#' + patternDefinitions.markerPortOut)),
+							$scope.nodeData,
+							patternDefinitions.markerPortOut
+						);
+
+                        if (!rectNode && !textNode && !portIn && !portOut) {
+							message('File "' + newType + '" isn\'t valid!', 'error');
+                        }
+                        element.attr('id', 'id_' + idNode);
+
+                        $scope.nodeData.portIn = portIn.data;
+                        $scope.nodeData.portOut = portOut.data;
+
+
+                        textNode.text($scope.nodeData.content);
+                        textNode.addClass('node_label');
+
+                        nodeWatcher($scope, rectNode);
+						nodeEventsHandler($scope, element, rectNode, idNode);
+                        portEventsHandler($scope, portIn, portOut, idNode);
+
+                        $scope.$emit('nodeInit', {
+							id: idNode
+						});
+
+                    });
+                }
+            })
+        }
+	}
+
+	function message(message, type) {
+		$scope.$emit('node_message', {
+			id: idNode,
+			type: type,
+			message: message
+		});
+	}
+
+	function portInit(base, port, data, marker) {
+
+		if (!port)
+			return null;
+		var baseRect = base[0].getBoundingClientRect();
+		var portRect = port[0].getBoundingClientRect();
+		var elemWidth = portRect.right - portRect.left;
+		var elemHeight = portRect.bottom - portRect.top;
+
+
+		var elemCenter = {
+			x: portRect.left - baseRect.left + elemWidth / 2,
+			y: portRect.top - baseRect.top + elemHeight / 2
+		}
+
+		var id = marker + '_' + data.id;
+		port.attr('id', id);
+
+		return {
+			element: port,
+			data: {
+				id: id,
+				offset: {
+					x: elemCenter.x,
+					y: elemCenter.y,
+				}
+			}
+		}
+	}
+
+	function nodeWatcher(scope, rectNode){
+		scope.$watch('nodeData.selected', function(newValue, oldValue) {
+			if (newValue) {
+				rectNode.addClass("node_active");
+				rectNode.attr('stroke-dasharray', '5,5');
+			} else {
+				rectNode.removeClass("node_active");
+				rectNode.attr('stroke-dasharray', '');
+			}
+		});
+	}
+
+	function nodeEventsHandler(scope, element, rectNode, idNode) {
+
+		element.on('mousedown', function (event) {
+			if (isPort || event.ctrlKey || event.button !== 0)
+				return;
+			var offsetMousePos = getOffsetPos(element, event);
+			scope.$emit('nodeMouseDown', {
+				id: idNode,
+				pos: {x: offsetMousePos.x, y: offsetMousePos.y}
+			});
+		});
+
+		element.on('mouseenter', function (event) {
+			rectNode.addClass("node_selected");
+		});
+
+		element.on('mouseleave', function (event) {
+			rectNode.removeClass("node_selected");
+		});
+
+		element.on('click', function (event) {
+			if (isPort || !event.ctrlKey)
+				return;
+			scope.$apply( function() {
+				scope.nodeData.selected = !scope.nodeData.selected;
+			});
+
+			scope.$emit('selectedItem', {
+				id: idNode,
+				type: 'node',
+				selected: scope.nodeData.selected
+			});
+		});
+
+		element.on('mouseup', function (event) {
+			if (event.ctrlKey)
+				return;
+			rectNode.removeClass("node_selected");
+			scope.$emit('nodeMouseUp', { });
+		});
+	}
+
+	function portEventsHandler(scope, portIn, portOut, idNode) {
+
+		portIn.element.on('mouseenter', function (event) {
+			isPort = true;
+			portIn.element.addClass("port_hovered");
+		});
+
+		portIn.element.on('mouseleave', function (event) {
+			isPort = false;
+			portIn.element.removeClass("port_hovered");
+		});
+
+		portIn.element.on('mouseup', function (event) {
+			scope.$emit('portInMouseUp', {
+				id: idNode,
+				pos: portIn.offset
+			});
+		});
+
+		portOut.element.on('mouseenter', function (event) {
+			isPort = true;
+			portOut.element.addClass("port_hovered");
+		});
+
+		portOut.element.on('mouseleave', function (event) {
+			isPort = false;
+			portOut.element.removeClass("port_hovered");
+		});
+
+		portOut.element.on('mousedown', function (event) {
+			if (event.button === 0) {
+				scope.$emit('portOutMouseDown', {
+					id: idNode,
+					pos: portOut.offset
+				});
+			}
+		});
+
+		portOut.element.on('mouseup', function (event) {
+			scope.$emit('portOutMouseUp', {
+				id: idNode,
+				pos: portOut.offset
+			});
+		});
+	}
+
+	function getOffsetPos(element, event) {
+		var elementRect = element[0].getBoundingClientRect();
+		return {x: event.clientX - elementRect.left, y: event.clientY - elementRect.top};
+	}
+}
+
+
+
