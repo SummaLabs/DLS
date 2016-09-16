9d1a6a6c51c11428e8259f160de14241e8bf898d
Alexander Kalinovsky (lab225ubu) <gakarak@gmail.com>
2016-09-16 19:31:17 +0300

    test-code for dataset-watcher

---
+++ app/backend-test/core_datasets/run04_test_dataset_watcher.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'ar'
+
+import os
+import json
+
+from app.backend.core.datasets.dbpreview import DatasetsWatcher
+
+pathWithDatasets='../../../data/datasets'
+
+if __name__ == '__main__':
+    dbWatcher = DatasetsWatcher(pathWithDatasets)
+    dbWatcher.refreshDatasetsInfo()
+    print (json.dumps(dbWatcher.getDatasetsInfoStatList(), indent=4) )
+    print ('----')
+    print (dbWatcher)
+
