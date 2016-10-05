59676e13698e6b4321b948cbd999270385a20745
Alexander Kalinovsky (arnb, Linux) <gakarak@gmail.com>
2016-10-05 08:57:47 +0300

    fix

--- app/backend/task/task_db_image2d_builder.py
+++ app/backend/task/task_db_image2d_builder.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ar'
 
+from app.backend.core import utils as dlsutils
 from app.backend.core.datasets.dbbuilder import DBImage2DBuilder, DBImage2DConfig
-from default_task import BaseTask
+from task import Task
 import time
 import random
 
-class TaskDBImage2DBuilder(BaseTask, DBImage2DBuilder):
+class TaskDBImage2DBuilder(Task, DBImage2DBuilder):
     def __init__(self, pathCfgInp=None, pathDirOut=None):
-        BaseTask.__init__(self)
+        Task.__init__(self)
         DBImage2DBuilder.__init__(self,pathCfgInp, pathDirOut)
+        self.text   = 'Image2D DB Builder'
+        self.type   = 'db-image2d-cls'
     def perform(self):
+        self.initializeInfo()
         while self.alive:
             time.sleep(2)
             self.progress += 10
