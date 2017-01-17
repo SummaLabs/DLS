910405ba29097e40f8d1cb7ce7864e4fed63e5f6
Alexander Kalinovsky (lab225ubu) <gakarak@gmail.com>
2017-01-17 19:09:15 +0300

    basic layer is done! Basic is: Input, Merge, Activation, Dense. ES6 is not a bad.

--- app/frontend/core_external/main.js
+++ app/frontend/core_external/main.js
@@ -2,6 +2,63 @@
  * Created by ar on 17.01.17.
  */
 
-let newVar = 'Hello, ES6!';
+import {LW_InputLayer, LW_Merge, LW_Flatten, LW_Dense, LW_Activation} from './models/lightweight_layers/layers_basic';
 
-console.log(`let-variable : ${newVar}`);
+
+const INPUT_SHAPE_1 = [null, 256,256,3];
+const INPUT_SHAPE_2 = [null, 256];
+
+/*
+ Simple test: Input Layer
+ */
+let inputLayer = new LW_InputLayer();
+let inputLayer_CalculatedShape_1 = inputLayer.get_output_shape_for(INPUT_SHAPE_1);
+let inputLayer_CalculatedShape_2 = inputLayer.get_output_shape_for(INPUT_SHAPE_2);
+
+console.log(`${inputLayer.toString()} ->calculatedShape(${INPUT_SHAPE_1}) : ${inputLayer_CalculatedShape_1}\n`);
+console.log(`${inputLayer.toString()} ->calculatedShape(${INPUT_SHAPE_2}) : ${inputLayer_CalculatedShape_2}\n\n\n`);
+
+/*
+ Simple test: Activation Layer
+ */
+let activationLayer = new LW_Activation();
+let activationLayer_CalculatedShape_1 = activationLayer.get_output_shape_for(INPUT_SHAPE_1);
+let activationLayer_CalculatedShape_2 = activationLayer.get_output_shape_for(INPUT_SHAPE_2);
+
+console.log(`${activationLayer.toString()} ->calculatedShape(${INPUT_SHAPE_1}) : ${activationLayer_CalculatedShape_1}\n`);
+console.log(`${activationLayer.toString()} ->calculatedShape(${INPUT_SHAPE_2}) : ${activationLayer_CalculatedShape_2}\n\n\n`);
+
+/*
+ Simple test: Dense Layer
+ */
+let denseLayer = new LW_Dense(128);
+try {
+    let denseLayer_CalculatedShape_1 = denseLayer.get_output_shape_for(INPUT_SHAPE_1);
+    console.log(`${denseLayer.toString()} ->calculatedShape(${INPUT_SHAPE_1}) : ${denseLayer_CalculatedShape_1}\n`);
+} catch (err) {
+    console.log(`Error: ${err}`);
+}
+
+let denseLayer_CalculatedShape_2 = denseLayer.get_output_shape_for(INPUT_SHAPE_2);
+console.log(`${denseLayer.toString()} ->calculatedShape(${INPUT_SHAPE_2}) : ${denseLayer_CalculatedShape_2}\n\n\n`);
+
+/*
+ Simple test: Merge Layer
+ */
+
+let mergeLayer1 = new LW_Merge({mode:'concat'});
+const INPUT_SHAPES_MERGE_1 = [[null,128], [null, 128]];
+const INPUT_SHAPES_MERGE_2 = [[null,256,256,3], [null, 256,256,1]];
+const INPUT_SHAPES_MERGE_3 = [[null,3, 256,256,256], [null, 1, 256, 256,256]];
+let mergeLayer_CalculatedShape_1 = mergeLayer1.get_output_shape_for(INPUT_SHAPES_MERGE_1);
+let mergeLayer_CalculatedShape_2 = mergeLayer1.get_output_shape_for(INPUT_SHAPES_MERGE_2);
+
+console.log(`${mergeLayer1.toString()} ->calculatedShape(${INPUT_SHAPES_MERGE_1}) : ${mergeLayer_CalculatedShape_1}\n`);
+console.log(`${mergeLayer1.toString()} ->calculatedShape(${INPUT_SHAPES_MERGE_2}) : ${mergeLayer_CalculatedShape_2}\n`);
+
+let mergeLayer2 = new LW_Merge({mode:'concat', concat_axis:1});
+for(let ii=1; ii<5; ii++) {
+    mergeLayer2.concat_axis = ii;
+    let mergeLayer_CalculatedShape_3 = mergeLayer2.get_output_shape_for(INPUT_SHAPES_MERGE_3);
+    console.log(`\tconcatAxis = ${ii}, ${mergeLayer2.toString()} ->calculatedShape(${INPUT_SHAPES_MERGE_3}) : ${mergeLayer_CalculatedShape_3}\n`);
+}
--- app/frontend/core_external/models/lightweight_layers/layers_basic.js
+++ app/frontend/core_external/models/lightweight_layers/layers_basic.js
@@ -7,17 +7,24 @@ export const default_dim_ordering = 'th';
 //////////////////////////////////////////////
 export class LW_Layer {
     constructor() {
-        this.input_shape=None;
+        this.input_shape=null;
     }
-    get_config() {
+    // FIXME: is this method really needed?
+    static get_config() {
         return null;
     }
     get_output_shape_for(input_shape) {
         return input_shape;
     }
     toString() {
-        let outputShape = this.get_output_shape_for(this.input_shape);
-        return `LW_Layer(input_shape=${this.input_shape}, output_shape=${outputShape})`;
+        const layerClassName = this.constructor.name;
+        let outputShape = null;
+        try{
+            outputShape = this.get_output_shape_for(this.input_shape);
+        } catch (err) {
+            outputShape = 'unknown';
+        }
+        return `${layerClassName}(input_shape=${this.input_shape}, output_shape=${outputShape})`;
     }
 }
 
@@ -30,3 +37,92 @@ export class LW_InputLayer extends LW_Layer {
 }
 
 //////////////////////////////////////////////
+
+
+export class LW_Merge extends LW_Layer {
+    constructor({mode='sum', concat_axis=-1, dot_axes=-1}) {
+        super();
+        this.mode = mode;
+        this.concat_axis = concat_axis;
+        this.dot_axes = dot_axes;
+    }
+    get_output_shape_for(input_shape) {
+        let input_shapes = input_shape;
+        if (['sum', 'mul', 'ave', 'max'].indexOf(this.mode)>0) {
+            // All tuples in input_shapes should be the same.
+            return input_shapes[0]
+        } else if(this.mode === 'concat') {
+            let output_shape = new Array(...input_shapes[0]); // make a copy
+            for (let shape of input_shapes.slice(1)) {
+                // fucking JS
+                let idxAxisOut = this.concat_axis;
+                let idxAxisShp = this.concat_axis;
+                if(this.concat_axis<0) {
+                    idxAxisOut = output_shape.length - 1;
+                    idxAxisShp = shape.length - 1;
+                }
+                //
+                if (output_shape[idxAxisOut] === null || shape[idxAxisShp] === null ) {
+                    output_shape[idxAxisOut] = null;
+                    break;
+                }
+                //FIXME: check "+= shape operation" --> Array concatenation?
+                output_shape[idxAxisOut] += shape[idxAxisShp];
+            }
+            return new Array(output_shape);
+        } else if(['dot', 'cos'].indexOf(this.mode)) {
+            let shape1 = new Array(...input_shapes[0]); // make a copy
+            let shape2 = new Array(...input_shapes[1]);
+            shape1.pop(this.dot_axes[0]);
+            shape2.pop(this.dot_axes[1]);
+            shape2.pop(0);
+            let output_shape = [...shape1,...shape2];
+            if(output_shape.length===1) {
+                output_shape = [...output_shape, ...[1]];
+            }
+            return output_shape;
+        }
+    }
+}
+
+//////////////////////////////////////////////
+export class LW_Flatten extends LW_Layer {
+    constructor() {
+        super();
+    }
+    get_output_shape_for(input_shape) {
+        /*
+        if not all(input_shape[1:]):
+            raise Exception('The shape of the input to "Flatten" '
+            'is not fully defined '
+            '(got ' + str(input_shape[1:]) + '. '
+            'Make sure to pass a complete "input_shape" '
+            'or "batch_input_shape" argument to the first '
+            'layer in your model.')
+         */
+        tprod = 1;
+        for (ii of input_shape.slice(1)) {
+            tprod += ii;
+        }
+        return [input_shape[0], tprod];
+    }
+}
+
+//////////////////////////////////////////////
+export class LW_Dense extends LW_Layer {
+    constructor(output_dim) {
+        super();
+        this.output_dim = output_dim;
+    }
+    get_output_shape_for(input_shape) {
+        if ( !(input_shape!=null && input_shape.length===2) ) {
+            throw new TypeError(`Invalid input shape: ${input_shape}`);
+        }
+        return [input_shape[0], this.output_dim];
+    }
+}
+
+//////////////////////////////////////////////
+export class LW_Activation extends LW_Layer {
+
+}
