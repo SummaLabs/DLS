3bbf25839384fae4d441b8112bb02ea452e239a7
sergo <sergokov@gmail.com>
2017-01-20 00:04:11 +0300

    Core Temp 2

--- app/backend/core/dataset/__init__.py
+++ app/backend/core/dataset/__init__.py
@@ -1,7 +1,55 @@
 class Dataset(object):
     def __init__(self, workspace, path):
         self.workspace = workspace
-        self._load_metadata(path)
+        self.metadata = self._load_metadata(path)
 
     def _load_metadata(self, path):
         print "Load Dataset from Path" + path
+
+    def load_batch(self, batch_size):
+        print ""
+
+
+class DataRecord(object):
+    def __init__(self):
+        self.data = self._load_data()
+
+    def _load_data(self):
+        print "Test"
+
+    def get_data_from_slot(self, slot_index):
+        print ""
+
+class Data(object):
+    def __init__(self):
+        print ""
+
+    def _load_data(self):
+        print "Test"
+
+    def to_data_frame(self):
+        raise NotImplementedError("Please Implement this method")
+
+
+class Image2DData(Data):
+    data_type = DataType.Image2D
+    def __init__(self):
+        print ""
+
+    def _load_data(self):
+        print "Test"
+
+
+class Metadata(object):
+    def __init__(self):
+        print ""
+
+
+class Image2DMetadata(Metadata):
+    data_type = DataType.Image2D
+    def __init__(self):
+        print ""
+
+
+class DataType:
+    Image2D, Image3D, Text = range(3)
