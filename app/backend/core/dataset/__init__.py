fbf4d5301a33c0f975e80a685d7b6eb40c8937f7
sergo <sergokov@gmail.com>
2017-01-22 23:24:41 +0300

    Temp core

--- app/backend/core/dataset/__init__.py
+++ app/backend/core/dataset/__init__.py
@@ -1,3 +1,7 @@
+from itertools import islice
+import csv, sys
+
+
 class Dataset(object):
     def __init__(self, workspace, path):
         self.workspace = workspace
@@ -10,15 +14,49 @@ class Dataset(object):
         print ""
 
 
-class DataRecord(object):
-    def __init__(self):
-        self.data = self._load_data()
+class Input(object):
+    def __init__(self, csv_file_path, with_header=False):
+        self.csv_file_path = csv_file_path
+        header_row = self._read_rows(self.csv_file_path, 1)[0]
+        if with_header:
+            self._columns = header_row
+        else:
+            self._columns = range(0, len(header_row))
 
-    def _load_data(self):
-        print "Test"
+    def _read_rows(self, csv_file_path, rows_number):
+        rows = []
+        with open(csv_file_path, 'rb') as f:
+            reader = csv.reader(f)
+            try:
+                for row in islice(reader, 0, rows_number):
+                    rows.append(row)
+            except csv.Error as e:
+                sys.exit('file %s, line %d: %s' % (csv_file_path, reader.line_num, e))
 
-    def get_data_from_slot(self, slot_index):
-        print ""
+        return rows
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @columns.setter
+    def columns(self, columns):
+        l = len(columns)
+        if (l > len(self._columns)) or (l < len(self._columns)):
+            raise Exception("Input columns size: %d is not compatible with existing data size: %d" % (l, len(self._columns)))
+        self._columns = columns
+
+    @property
+    def column_by_index(self, index):
+        return self._columns[index]
+
+    @column_by_index.setter
+    def column_by_index(self, index, name):
+        self._columns[index] = name
+
+    def print_columns(self):
+        print self._columns
+
 
 class Data(object):
     def __init__(self):
@@ -32,7 +70,6 @@ class Data(object):
 
 
 class Image2DData(Data):
-    data_type = DataType.Image2D
     def __init__(self):
         print ""
 
@@ -46,10 +83,14 @@ class Metadata(object):
 
 
 class Image2DMetadata(Metadata):
-    data_type = DataType.Image2D
     def __init__(self):
         print ""
 
 
 class DataType:
     Image2D, Image3D, Text = range(3)
+
+
+if __name__ == '__main__':
+    input = Input("/home/sergo/Work/Gitlab/DLS/data-test/idx-simple4c_test-all.csv", with_header=True)
+    input.print_columns()
