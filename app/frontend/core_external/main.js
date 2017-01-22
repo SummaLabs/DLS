080f92dfc032501124484578312fc2d7812aae8e
Alexander Kalinovsky (arnb, Linux) <gakarak@gmail.com>
2017-01-22 12:52:04 +0300

    flow parsing in progress

--- app/frontend/core_external/main.js
+++ app/frontend/core_external/main.js
@@ -2,27 +2,13 @@
  * Created by ar on 17.01.17.
  */
 
-import {test_layers_basic}          from './main_test_basic';
-import {test_layers_convolutional}  from './main_test_convolutional';
-import {test_layers_pooling}        from './main_test_pooling';
+angular.module('DemoNetworkFlowParser', ['ngMaterial'])
+    .controller('ControllerFlowParsing', ControllerFlowParsing);
 
-
-// (1) Test Basic layers
-console.log('\n\n****************** [    TEST BASIC LAYERS    ] ******************');
-test_layers_basic();
-
-// (2) Test Convolutional layers
-console.log('\n\n****************** [ TEST BASIC CONVOLUTIONAL ] ******************');
-test_layers_convolutional();
-
-// (3) Test Pooling layers
-console.log('\n\n****************** [    TEST BASIC Pooling    ] ******************');
-test_layers_pooling();
-
-
-
-
-
+function ControllerFlowParsing($scope) {
+    var self = this;
+    $scope.testArray = [1,2,3,4,5,6,7];
+}
 
 
 
--- app/frontend/core_external/run02-babelify-main.sh
+++ app/frontend/core_external/run02-babelify-main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./node_modules/.bin/browserify main.js --transform babelify --debug --outfile main-compiled.js
+./node_modules/.bin/browserify main_lw_layers.js --transform babelify --debug --outfile main_lw_layers-compiled.js
