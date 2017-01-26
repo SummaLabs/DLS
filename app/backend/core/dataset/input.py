063f4a3bf096f7d4be1202b9faa2d22cc0dc23b1
sergo <sergokov@gmail.com>
2017-01-27 01:00:17 +0300

    Temp core dataset 3

--- app/backend/core/dataset/input.py
+++ app/backend/core/dataset/input.py
@@ -5,14 +5,14 @@ import csv, sys
 class Schema(object):
     def __init__(self, csv_file_path, header=False):
         self.csv_file_path = csv_file_path
-        header_row = self.read_n_rows(self.csv_file_path, 1)[0]
+        header_row = Schema._read_n_rows(self.csv_file_path, 1)[0]
         if header:
-            self._columns = header_row
+            self._columns = [Schema.Column(item, [index]) for index, item in enumerate(header_row)]
         else:
-            self._columns = [ 'col_' + str(index) for index in range(0, len(header_row))]
+            self._columns = [Schema.Column('col_' + str(index), [index]) for index in range(0, len(header_row))]
 
     @staticmethod
-    def read_n_rows(csv_file_path, rows_number):
+    def _read_n_rows(csv_file_path, rows_number):
         rows = []
         with open(csv_file_path, 'rb') as f:
             reader = csv.reader(f)
@@ -24,9 +24,32 @@ class Schema(object):
 
         return rows
 
-    def __setitem__(self, key, item):
-        index = self._columns.index(key)
-        self._columns[index] = item
+    class Column(object):
+        def __init__(self, name, columns_indexes):
+            self._name = name
+            # CSV corresponding columns indexes
+            self._columns_indexes = columns_indexes
+
+        @property
+        def name(self):
+            return self._name
+
+        @name.setter
+        def name(self, name):
+            self._name = name
+
+        @property
+        def columns_indexes(self):
+            return self._columns_indexes
+
+        @columns_indexes.setter
+        def columns_indexes(self, columns_indexes):
+            self._columns_indexes = columns_indexes
+
+    def __setitem__(self, old_name, new_name):
+        for column in self._columns:
+            if column.name == old_name:
+                column.name = new_name
 
     @property
     def columns(self):
@@ -36,21 +59,27 @@ class Schema(object):
     def columns(self, columns):
         l = len(columns)
         if (l > len(self._columns)) or (l < len(self._columns)):
-            raise Exception("Passed columns number: %d is not compatible with current columns number: %d" % (l, len(self._columns)))
-        self._columns = columns
+            raise Exception("Passed columns number: %d is not compatible with Schema current columns number: %d" % (l, len(self._columns)))
+        for index, item in enumerate(columns):
+            self._columns[index].name = item
 
     def drop_column(self, column_name):
+        for column in list(self._columns):
+            if column.name == column_name:
+                self._columns.remove(column)
+
+    def merge_columns(self, new_column_name, columns_to_merge = []):
         pass
 
-    def merge_columns(self, column_names):
+    def merge_columns_in_range(self, new_column_name, range = []):
         pass
 
     def print_columns(self):
-        print ", ".join([col for col in self._columns])
+        print ", ".join([col.name for col in self._columns])
 
     def print_data(self):
         print "First 10 records:"
-        for row in self.read_n_rows(self.csv_file_path, 10):
+        for row in Schema._read_n_rows(self.csv_file_path, 10):
             print row
 
 
