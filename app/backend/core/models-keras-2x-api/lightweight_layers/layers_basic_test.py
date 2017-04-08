844e823e6e6b3e1e080f1917d98e86ee56728a5d
Alexander Kalinovsky (deepw) <gakarak@gmail.com>
2017-04-08 13:12:41 +0300

    tests in progress...

--- app/backend/core/models-keras-2x-api/lightweight_layers/layers_basic_test.py
+++ app/backend/core/models-keras-2x-api/lightweight_layers/layers_basic_test.py
@@ -1,13 +1,25 @@
-import pytest
+import unittest
 
 from layers_basic import LW_Merge
 
 
-def test_merge_shape():
+from keras.models import Model
+from keras.layers import merge, Input
 
-    merge_layer = LW_Merge(mode='concat')
-    assert merge_layer.input_shape == 4
+class TestBasicLWLayers(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_merge_1d(self):
+        inp1 = Input(shape=(8))
+        inp2 = Input(shape=(3))
+        model = Model(inputs=[inp1, inp2])
+
+
+        merge_layer = LW_Merge(mode='concat')
+        assert merge_layer.input_shape == 4
 
 
 if __name__ == '__main__':
-  pytest.main([__file__])
+    unittest.main()
