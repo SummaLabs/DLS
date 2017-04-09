06f9f1a3b5d308b765e967eb4d22760de99e217e
Alexander Kalinovsky (Banuba, MacBook) <gakarak@gmail.com>
2017-04-09 12:14:49 +0300

    test script form new Keras 2x API model config

--- app/backend-test/keras_2x_api/run01_print_keras_model_json.py
+++ app/backend-test/keras_2x_api/run01_print_keras_model_json.py
@@ -2,5 +2,24 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ar'
 
+import json
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPool2D, Flatten, Dense
+
 if __name__ == '__main__':
-    pass
+    inpData = Input(shape=(3,128,128))
+    x = Conv2D(filters=8,  kernel_size=(3, 3), activation='relu')(inpData)
+    x = MaxPool2D(pool_size=(2,2))(x)
+    #
+    x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu')(x)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    #
+    x = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(x)
+    x = MaxPool2D(pool_size=(2, 2))(x)
+    #
+    x = Flatten()(x)
+    x = Dense(units=128, activation='relu')(x)
+    x = Dense(units=2, activation='softmax')(x)
+    model = Model(inputs=[inpData], outputs=[x])
+    modelJson = model.to_json()
+    print (json.dumps(json.loads(modelJson), indent=4))
